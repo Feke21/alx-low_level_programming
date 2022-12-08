4e6b1b5633ce62d0fb9e6c3546d18a8817521c06
Feke_21 <favouredubio@gmail.com>
2022-12-08 15:57:40 -0800

    Print sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,4 +16,5 @@ printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(l));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(g));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+return (0);
 }
