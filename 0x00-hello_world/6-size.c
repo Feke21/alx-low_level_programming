c6229c817ab6692e6bbe38b1ce1ac143418352cb
Feke_21 <favouredubio@gmail.com>
2022-12-08 15:54:20 -0800

    Print sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,9 +11,9 @@ int i;
 long int l;
 long long int g;
 float f;
-printf("Size of a char: %lu.\n", (unsigned long)sizeof(c));
-printf("Size of an int: %lu.\n", (unsigned long)sizeof(i));
-printf("Size of a long int: %lu.\n", (unsigned long)sizeof(l));
-printf("Size of a long long int: %lu.\n", (unsigned long)sizeof(g));
-printf("Size of a float: %lu.\n", (unsigned long)sizeof(f));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(l));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(g));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 }
