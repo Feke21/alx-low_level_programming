8a720ea149209532df9bedb22d90dab1aaf9572e
Feke_21 <favouredubio@gmail.com>
2022-12-08 17:11:38 -0800

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+dprintf(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
