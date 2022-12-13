8d7ede6654f218b81dc87f65946feba48c719a7d
Feke_21 <favouredubio@gmail.com>
2022-12-13 13:17:18 +0000

    Print putchar text

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int letter;
+
+	letter = '_';
+	putchar(letter);
+	letter = 'p';
+	putchar(letter);
+	letter = 'u';
+	putchar(letter);
+	letter = 't';
+	putchar(letter);
+	letter = 'c';
+	putchar(letter);
+	letter = 'h';
+	putchar(letter);
+	letter = 'a';
+	putchar(letter);
+	letter = 'r';
+	putchar(letter);
+
+	putchar('\n');
+	return (0);
+}
