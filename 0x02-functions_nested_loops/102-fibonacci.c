b9fcb47eee024f02636aa3146ccb74430cf415a3
Feke_21 <favouredubio@gmail.com>
2022-12-14 08:34:25 +0000

    Print first 50 fibonacci sequence

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,18 +8,18 @@
  */
 int main(void)
 {
-	int l, fs;
+	int l;
 	/* initialize the first 2 terms */
-	int t1, t2;
+	long int t1, t2, fs;
 	/* print the first 2 terms in sequence */
 	t1 = 1;
 	t2 = 2;
-	printf("%d, %d", t1, t2);
+	printf("%ld, %ld", t1, t2);
 	/* print the remaining 48 terms in sequence */
 	for (l = 0; l < 48; l++)
 	{
 		fs = t1 + t2;
-		printf(", %d", fs);
+		printf(", %ld", fs);
 		t1 = t2;
 		t2 = fs;
 	}
