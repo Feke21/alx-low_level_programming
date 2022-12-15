ff2f6006e7783ef4e4809d3c421ada13c856950d
Feke_21 <favouredubio@gmail.com>
2022-12-15 22:46:56 +0000

    Fixing fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,27 +6,28 @@
  */
 int main(void)
 {
-	int n;
+	int i;
 
-	for (n = 1; n <= 100; n++)
+	for (i = 1; i < 100; i++)
 	{
-		if ((n % 3 == 0) && (n % 5 == 0))
+		if (i % 15 == 0)
 		{
 			printf("FizzBuzz ");
 		}
-		else if (n % 3 == 0)
+		else if (i % 3 == 0)
 		{
 			printf("Fizz ");
 		}
-		else if (n % 5 == 0)
+		else if (i % 5 == 0)
 		{
 			printf("Buzz ");
 		}
 		else
 		{
-			printf("%d ", n);
+			printf("%d ", i);
 		}
 	}
+	printf("Buzz");
 	printf("\n");
 	return (0);
 }
