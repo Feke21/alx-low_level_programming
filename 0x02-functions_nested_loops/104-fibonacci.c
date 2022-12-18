e09420a9b0c3bc70577045cbb176e69b150f70ee
Feke_21 <favouredubio@gmail.com>
2022-12-18 17:12:42 +0000

    Fixing 98 fibonacci sequence

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,16 +8,16 @@
 int main(void)
 {
 	int i;
-	long int a, b, a1, a2, b1, b2;
+	unsigned long int a, b, a1, a2, b1, b2;
 
 	a = 1;
 	b = 2;
 
-	printf("%ld", a);
+	printf("%lu", a);
 
 	for (i = 1; i < 91; i++)
 	{
-		printf(", %ld", b);
+		printf(", %lu", b);
 		b = b + a;
 		a = b - a;
 	}
@@ -29,8 +29,8 @@ int main(void)
 
 	for (i = 92; i < 99; ++i)
 	{
-		printf(", %ld", b1 + (b2 / 1000000000));
-		printf("%ld", b2 % 1000000000);
+		printf(", %lu", b1 + (b2 / 1000000000));
+		printf("%lu", b2 % 1000000000);
 		b1 = b1 + a1;
 		a1 = b1 - a1;
 		b2 = b2 + a2;
