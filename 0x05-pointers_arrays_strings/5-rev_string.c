ed5b707376da8794b5a52dc293bbb2e96af4ca4a
Feke_21 <favouredubio@gmail.com>
2022-12-19 19:43:49 +0000

    Reverse a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -15,7 +15,7 @@ void rev_string(char *s)
 	for (i = 0; i < len / 2; i++)
 	{
 		temp = s[i];
-		s[i] = s[len -i -1];
-		s[len -i -1] = temp;
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
 	}
 }
