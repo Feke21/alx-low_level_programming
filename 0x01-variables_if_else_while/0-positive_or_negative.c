1461a02b92241d1e9fc2717e2f858f5636f68c98
Feke_21 <favouredubio@gmail.com>
2023-01-03 23:22:18 +0100

    checking using git bash

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,9 +12,14 @@
 int main(void)
 {
 int n;
-if (n > 0)
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+if (n>0){
 printf("%d is positive\n", n);
-else if (n == 0)
+}
+else if (n==0)
 {
 printf("%d is zero\n", n);
 }
@@ -22,8 +27,5 @@ else
 {
 printf("%d is negative\n", n);
 }
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-/* your code goes there */
 return (0);
 }
