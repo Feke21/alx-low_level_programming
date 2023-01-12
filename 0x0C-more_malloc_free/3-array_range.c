5565d09e557ab1d927bf791561d81df137cb7d5a
Feke_21 <favouredubio@gmail.com>
2023-01-12 18:04:24 +0000

    Create array of integers

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -18,7 +18,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	ar = malloc(sizeof(int) * (max - min) + 1);
+	ar = malloc(sizeof(int) * ((max - min) + 1));
 	if (ar == NULL)
 		return (NULL);
 	for (i = 0; min <= max; i++, min++)
