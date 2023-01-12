bbe7ef276f448e3aed43335266161a25f74a7cda
Feke_21 <favouredubio@gmail.com>
2023-01-12 18:36:33 +0000

    Reallocates memory block

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -8,20 +8,25 @@
  * @old_size: size,in bytes,of the allocated space for ptr
  * @new_size:new size,in bytes of the new memory block
  *
- * Return: if new_size > old_size,the “added” memory should not be initialized
+ * Return: if new_size > old_size,memory should not be initialized
  * if new_size == old_size. Return ptr
  * if ptr is null then ptr= malloc(new_size)
  * if new_size = 0 and ptr is not NULL then free(ptr). Return null
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
 	if (new_size == old_size)
 		free(ptr);
 	if (ptr == NULL)
 		ptr = malloc(new_size);
 	if (new_size == old_size)
 		return (ptr);
-	free (ptr);
+	free(ptr);
 	ptr = malloc(new_size);
 	return (ptr);
 }
