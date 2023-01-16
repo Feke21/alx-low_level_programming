e51689848bba583ca5fcfa5fbe8e0c6821c9bdf4
Feke_21 <favouredubio@gmail.com>
2023-01-16 21:37:19 +0000

    Fixing file accord. to Betty

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - struct for dog header
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ */
 struct dog
 {
 	char *name;
