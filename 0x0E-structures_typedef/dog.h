8a748bb9b3a482fa45edbebd64047fb94a6be22c
Feke_21 <favouredubio@gmail.com>
2023-01-16 23:00:53 +0000

    Fixing accord. to Betty

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,14 +8,14 @@
  * @owner: third member
  *
  * Description: struct called dog that stores its
- * name, age and owner 
+ * name, age and owner
  */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} ;
+};
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
