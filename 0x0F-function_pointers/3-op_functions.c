d75d40bf3a49b9e7c8e2feca6e007409e51ee2ec
Feke_21 <favouredubio@gmail.com>
2023-01-18 19:39:45 +0000

    Adding functions file

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+/**
+ * op_add - adds numbers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: result
+ */
+int op_add(int a, int b)
+{
+	return (a + b)
+}
+
+/**
+ * op_sub - subtracts numbers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies numbers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides numbers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - remainder of division
+ * @a: first int
+ * @b: second int
+ *
+ * Return: result
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
