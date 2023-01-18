6dfc556b37634cb4cc35bcfa3db9e7366e6b64dd
Feke_21 <favouredubio@gmail.com>
2023-01-18 13:56:03 +0000

    Print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints name
+ * @name: name
+ * @f: function pointer
+ *
+ * Return: nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+	{
+		f(name);
+	}
+}
