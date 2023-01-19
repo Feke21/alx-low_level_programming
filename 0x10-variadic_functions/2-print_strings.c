57892f1aef1143598f2537008f2a5ed2b0662ee3
Feke_21 <favouredubio@gmail.com>
2023-01-19 15:22:47 +0000

    Fixing code

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,7 +22,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (str)
 			printf("%s", str);
 		else
-			printf("(nil");
+			printf("(nil)");
 
 		if (i < n - 1)
 			if (separator)
