28aa31e8870287b19868902fcefe8d2abe0f2478
Feke_21 <favouredubio@gmail.com>
2023-01-19 16:28:11 +0000

    Fixing print all accord. to Betty

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -23,17 +23,16 @@ void print_all(const char * const format, ...)
 			{
 				printf(", ");
 				break;
-			}
-			j++;
+			} j++;
 		}
 		switch (format[i])
 		{
 			case 'c':
 				printf("%c", va_arg(valist, int)), k = 1;
-			       	break;
+				break;
 			case 'i':
 				printf("%d", va_arg(valist, int)), k = 1;
-		 		break;
+				break;
 			case 'f':
 				printf("%f", va_arg(valist, double)), k = 1;
 				break;
@@ -46,9 +45,6 @@ void print_all(const char * const format, ...)
 				}
 				printf("%s", str);
 				break;
-		}
-		i++;
+		} i++;
 	}
-	printf("\n");
-	va_end(valist);
-}	
+	printf("\n"), va_end(valist); }
