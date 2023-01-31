6b2d3e9f7f004c43eed163eb19c63354e7a0ca5c
Feke_21 <favouredubio@gmail.com>
2023-01-31 15:43:58 +0000

    Fixing

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -4,12 +4,13 @@
  * print_listint_safe - prints a listint_t linked list
  * @head: head of singly linked list
  *
- * Return: number of nodes in list(i)
+ * Return: number of nodes in list (i)
  */
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t i = 0;
-	const listint_t *temp, *current;
+	const listint_t *temp;
+	const listint_t *current;
 
 	current = head;
 	while (current != NULL)
