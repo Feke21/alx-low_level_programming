d35057414bbe837547323762c388e15c678ac027
Feke_21 <favouredubio@gmail.com>
2023-01-31 14:49:30 +0000

    Print listint_t linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,8 +9,7 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t i = 0;
-	const listint_t *temp;
-	const listint_t *current;
+	const listint_t *temp, *current;
 
 	current = head;
 	while (current != NULL)
