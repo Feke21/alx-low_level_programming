d866b3f76d7613690738929caf5dfd0faea881a6
Feke_21 <favouredubio@gmail.com>
2023-03-16 19:43:05 +0000

    Length of list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the number of nodes in a doubly linked list
+ * @h: pointer to the list
+ *
+ * Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t node = 0;
+
+	while (h != NULL)
+	{
+		node++;
+		h = h->next;
+	}
+	return (node);
+}
