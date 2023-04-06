c15cde1792ea120019a9ec88e60830e928dad54d
Feke_21 <favouredubio@gmail.com>
2023-04-06 12:05:59 +0000

    Adding header file and hash table create file

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ *
+ * Return: a pointer to newly created hash table. null if it fails
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+	hash_node_t **array;
+	unsigned long int i;
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+	       return (NULL);
+
+	array = malloc(sizeof(hash_node_t *) * size);
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		array[i] = NULL;
+
+	ht->array = array;
+	ht->size = size;
+
+	return (ht);
+}	
